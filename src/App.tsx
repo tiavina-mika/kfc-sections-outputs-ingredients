f09a54c327e7c34a3d44ccd87207457259e0a5cb
tiavina-mika <tiavinamika@gmail.com>
2025-06-04 10:58:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, CssBaseline } from '@mui/material'
+import AppBar from './components/AppBar'
+import Footer from './components/Footer'
+import Sections from './containers/Sections'
+import { recipe } from './utils/data'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: "column", flexGrow: 1 }}>
+      <CssBaseline />
+      <AppBar />
+      <Box sx={{ p: 4 }}>
+        <Sections sections={recipe.sections} />
+      </Box>
+      <Footer />
+    </Box>
   )
 }
 
