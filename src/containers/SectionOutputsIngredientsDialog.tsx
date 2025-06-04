31c6c2b5bc2ab38ffbdcd1f0cf31b2a19d829459
tiavina-mika <tiavinamika@gmail.com>
2025-06-04 13:49:00 +0300

    Update SectionOutputsIngredientsDialog.tsx

--- src/containers/SectionOutputsIngredientsDialog.tsx
+++ src/containers/SectionOutputsIngredientsDialog.tsx
@@ -9,7 +9,16 @@ import SectionOutputsIngredientsForm, { type SectionOutputsIngredientsFormValues
 import type { FormikProps } from "formik";
 import { useRef } from "react";
 
-
+const sx = {
+  dialog: {
+    " & .MuiDialog-paper": {
+      display: "flex",
+      flexDirection: "column",
+      gap: "32px",
+      padding: "32px",
+    }
+  }
+}
 type Props = {
   open: boolean;
   onClose: () => void;
@@ -36,16 +45,16 @@ const SectionOutputsIngredientsDialogForm = ({
   };
 
 return (
-  <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-    <DialogTitle>
+  <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth sx={sx.dialog}>
+    <DialogTitle sx={{ px: 0 }}>
       Divisez votre section en répartissant les ingrédients dans chaque partie
       et attribuez-leur un nom.
     </DialogTitle>
-    <DialogContent>
+    <DialogContent sx={{ px: 0 }}>
       <SectionOutputsIngredientsForm ref={formikRef} onSubmit={handleValidate} section={section} />
     </DialogContent>
-    <DialogActions>
-      <Button onClick={onClose} variant="outlined">
+    <DialogActions sx={{ justifyContent: "space-between", px: 0 }}>
+      <Button onClick={onClose} variant="text">
         Retour
       </Button>
       <Button type="submit" variant="contained" color="primary" onClick={handleConfirm}>
