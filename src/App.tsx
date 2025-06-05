eeb984357e9b348a825fe28d0d33b921e5df996a
tiavina-mika <tiavinamika@gmail.com>
2025-06-05 20:05:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, CssBaseline } from '@mui/material'
+import { Box, CssBaseline, Typography } from '@mui/material'
 import AppBar from './components/AppBar'
 import Footer from './components/Footer'
 import Sections from './containers/Sections'
@@ -10,6 +10,17 @@ const App = () => {
       <CssBaseline />
       <AppBar />
       <Box sx={{ p: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Open the modal to split a section by ingredients
+        </Typography>
+        <Box component="ul" sx={{ pl: 4 }}>
+          <Typography component="li">Modal label: Split your section by distributing ingredients into each part and giving them a name.</Typography>
+          <Typography component="li">You can add a new part.</Typography>
+          <Typography component="li">Part names are editable text inputs.</Typography>
+          <Typography component="li">You can drag and drop ingredients from one part to another.</Typography>
+          <Typography component="li">If a part has no ingredients, a "skeleton" is displayed instead.</Typography>
+          <Typography component="li">You can delete all parts except the first two.</Typography>
+        </Box>
         <Sections sections={recipe.sections} />
       </Box>
       <Footer />
