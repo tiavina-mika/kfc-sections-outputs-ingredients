8386b72d8a1c7b669e803008918ba50e7781f374
tiavina-mika <tiavinamika@gmail.com>
2025-06-05 11:57:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import AppBar from './components/AppBar'
 import Footer from './components/Footer'
 import Sections from './containers/Sections'
 import { recipe } from './utils/data'
+// import { getAllIngredientsFromSection } from './utils/step.utils'
 
 const App = () => {
+  // console.log("supplier items", getAllIngredientsFromSection(recipe.sections[0]));
   return (
     <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: "column", flexGrow: 1 }}>
       <CssBaseline />
